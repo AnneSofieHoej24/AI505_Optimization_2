8f464e3038cdc8af88a27b43567355c59eb994e5
AnneSofieHoej24 <annesofie6960@hotmail.com>
2026-05-20 08:59:02 +0200

    Data + start

---
+++ asg2/src/T.py
@@ -0,0 +1,30 @@
+import random
+
+from roar_net_api.algorithms import greedy_construction
+
+class Team:
+
+ 
+    #def __init__
+
+ 
+    #def objective_value
+ 
+    #def lower_bound
+
+    #def assign
+
+class AssignMove:
+ 
+    #def __init__
+ 
+    #def apply_move
+ 
+    #def lower_bound_increment
+
+class AssignmentNeighbourhood:
+
+ 
+    #def moves
+ 
+    #def random_move
